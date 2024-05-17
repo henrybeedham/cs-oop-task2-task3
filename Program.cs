73a190b7c898986e52c7810f9e46fa73181797a7
Henry Beedham <45268597+henrybeedham@users.noreply.github.com>
2024-05-17 10:27:48 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Here is your empty program!
+        Console.WriteLine("Hello world!");
+    }
+}
+
+// 	Animal = CLASS
+// 	   Public
+// 	      Constructor (s, n)
+// 	      Procedure feed()
+// 	      Function getState()
+// 	      Function getSize()
+// 	   Private
+// 	      state: String 
+// 	      size: Integer
+	 
+// 	Animal.Constructor (s, n)
+// 	   Animal.state  s
+// 	   Animal.size  n
+ 
+// 	Procedure Animal.feed()
+// 	   Animal.size  Animal.size + 1
+// 	   IF Animal.size = 5 THEN
+// 	      Animal.state  "FISH"
+	 
+// 	Function Animal.getState
+// 	   RETURN Animal.state
+	 
+// 	Function Animal.getSize
+// 	   RETURN Animal.size
+	 
+// END CLASS
